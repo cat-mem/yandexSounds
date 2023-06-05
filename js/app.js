3a3f8381583b2dd7168ca20e2904bb1c3e7ca066
cat-mem <115585790+cat-mem@users.noreply.github.com>
2023-06-05 22:59:23 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,44 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true
+})
+
+gsap.fromTo('.hero-content', { opacity: 1 }, {
+    opacity: -1, scrollTrigger: {
+        trigger: '.hero-content',
+        start: 'center',
+        end: 'bottom',
+        scrub: true
+    }
+})
+
+gsap.fromTo('.container__left .gallery__item', { x: -100, y: 50, opacity: 0 }, {
+    opacity: 2, x: 0, y: 0, scrollTrigger: {
+        trigger: '.gallery__item',
+        scrub: true
+    }
+})
+
+gsap.fromTo('.container__right .gallery__item', { x: 100, y: 50, opacity: 0 }, {
+    opacity: 2, x: 0,y: 0, scrollTrigger: {
+        trigger: '.gallery__item',
+        scrub: true
+    }
+})
+
+
+
+
+
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+            --move-x: ${( e.clientX - window.innerWidth / 2 ) * -.005}deg;
+            --move-y: ${( e.clientY - window.innerHeight / 2 ) * -.01}deg;
+        `
+    })
+})
